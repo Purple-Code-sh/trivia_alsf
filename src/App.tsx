70682a3289b1c8e2aef4eb748555f0acd13c7495
Purple-Code-sh <purple.code.sh@gmail.com>
2025-04-06 18:34:21 -0600

    change "ley del efecto" texts

--- src/App.tsx
+++ src/App.tsx
@@ -76,9 +76,9 @@ function App() {
           <div className="absolute top-28 -right-28 lg:right-28 xl:right-72 -z-10 w-80 h-80 animate-pulse rounded-full bg-gradient-to-tr from-background via-primary to-secondary opacity-40 blur-3xl" />
 
           <div className='container flex flex-col lg:flex-row py-6 mx-auto gap-0'>
-            <div className='w-full lg:w-1/2 flex flex-col justify-center items-center lg:items-start gap-8 sm:gap-16'>
-              <h1 className='text-secondary mb-2 lg:mb-4 xl:mb-6 text-center leading-20 lg:text-left font-playwrite font-bold text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl'>
-                Ley del <span className='text-accent-orange'>Efecto</span>
+            <div className='w-full lg:w-1/2 flex flex-col justify-center items-center lg:items-start gap-8 sm:gap-12'>
+              <h1 className='text-secondary mb-2 lg:mb-4 xl:mb-6 text-center leading-16 md:leading-20 lg:leading-24 2xl:leading-32 lg:text-left font-playwrite font-bold text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl'>
+                Aprendizaje <span className='text-accent-orange'>Experiencial</span>
               </h1>
 
               <h2 className='text-light-300 text-center lg:text-left font-semibold text-2xl md:text-3xl lg:text-4xl'>
@@ -86,7 +86,7 @@ function App() {
               </h2>
 
               <h2 className='max-w-[420px] text-center lg:text-left md:max-w-[500px] lg:max-w-[600px] text-base md:text-lg xl:text-xl self-center lg:self-start'>
-                Este es un cuestionario en el que pones a prueba tus conocimientos sobre la ley del efecto.<br /> Tomalo con calma y no te preocupes por las respuestas correctas o incorrectas. Solo diviertete 😄
+                Este es un cuestionario en el que pones a prueba tus conocimientos sobre el aprendizaje experiencial.<br /> Tomalo con calma y no te preocupes por las respuestas correctas o incorrectas. Solo diviertete 😄
               </h2>
 
               <div className="w-full mx-auto   backdrop-blur-sm rounded-3xl">
